0d27d39b7029c5455bb106e9f71b59a6f9de9a98
MariaAcasia <mariaeduardaacasia@gmail.com>
2026-06-01 20:43:11 -0300

    Add files via upload

---
+++ aula1305/meuform.js
@@ -0,0 +1,7 @@
+let inputNome = document.getElementById('nome').value;
+let inputNome2 = document.getElementById('nome').value;
+
+console.log("1º variável: " + inputNome);
+console.log("2º variável: " + inputNome2);
+inputNome2.value = "Um novo valor";
+inputNome2.style.backgroundColor = "yellow";
